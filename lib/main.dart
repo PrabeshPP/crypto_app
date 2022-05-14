08a283f1f39b0b77bd0b9a066b8de30eff1fff21
Prabesh <prabesh.bista369@gmail.com>
2022-05-14 18:05:14 +0545

    welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const WelcomePage(),
       themeMode: ThemeMode.light,
       theme: AppThemeData.lightTheme(),
--- lib/view/onboarding_screen/welcome_page.dart
+++ lib/view/onboarding_screen/welcome_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -9,13 +8,28 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-          
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0.0),
+        child: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+        ),
       ),
-      body: Container(
-        height: size.height,
-        width: size.width,
-        color:const Color.fromARGB(255, 71, 101, 249),
+      body: Stack(
+        children:[ Container(
+          height: double.infinity,
+          width: size.width,
+          color: const Color.fromARGB(255, 71, 101, 249),
+        ),
+        Positioned(
+          child: SizedBox(
+          height: size.height*0.3,
+          width: size.width*0.9,
+          child:SvgPicture.asset("assets/Image.svg",
+          fit: BoxFit.fill,) ,
+        ))
+        ],
       ),
     );
   }
